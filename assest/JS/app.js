2eb3845fa070940a3568c4d70aa7460d631bd551
Raihan hosen <83702632+raihanhosen011@users.noreply.github.com>
2021-08-29 18:08:25 +0600

    Delete assest directory

--- assest/JS/app.js
+++
@@ -1,7 +0,0 @@
-let header = document.getElementById("header");
-
-// ===== Header animation =======
-window.addEventListener("scroll", function() {
-    if (window.pageYOffset > 50) header.classList.add("is-sticky");
-    else header.classList.remove("is-sticky");
-});
